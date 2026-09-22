33fe00f90a69d1d822d6188ab445e610f61a83a5
OMAR AKHJI <omar_akhji@um5.ac.ma>
2026-09-22 23:25:00 +0000

    chore: remove temporary inspect_page.ts script

--- inspect_page.ts
+++
@@ -1,8 +0,0 @@
-async function main() {
-  const res = await fetch("http://localhost:4321/quiz/b1/lesen/1");
-  const text = await res.text();
-  const start = text.indexOf('id="page-content"');
-  console.log("PAGE-CONTENT HTML:");
-  console.log(text.slice(start, start + 2500));
-}
-main();
